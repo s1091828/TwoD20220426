fb4c15f443bc08ff366c38e74c5640762eab4fb8
Student <s1091828@gm.pu.edu.tw>
2022-04-26 09:36:59 +0800

    v3 : 使用building

--- app/src/main/java/com/example/twod/MainActivity.kt
+++ app/src/main/java/com/example/twod/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.twod
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.twod.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    var secondsLeft:Int = 1000  //倒數
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.txv.text = secondsLeft.toString()
+        binding.btnStart.isEnabled = true
+        binding.btnStop.isEnabled = false
     }
 }
