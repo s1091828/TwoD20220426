ae9afaabd4ae33f5daec5fb42248d4b186631ea2
Student <s1091828@gm.pu.edu.tw>
2022-04-26 10:23:49 +0800

    v4 : 倒數計時開始和暫停

--- app/src/main/java/com/example/twod/MainActivity.kt
+++ app/src/main/java/com/example/twod/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.twod
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.twod.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
     var secondsLeft:Int = 1000  //倒數
+    lateinit var job: Job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +21,32 @@ class MainActivity : AppCompatActivity() {
         binding.txv.text = secondsLeft.toString()
         binding.btnStart.isEnabled = true
         binding.btnStop.isEnabled = false
+
+        binding.btnStart.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(v: View?) {
+                //binding.txv.text = "開始倒數計時"
+
+                job = GlobalScope.launch(Dispatchers.Main) {
+                    while(secondsLeft > 0) {
+                        secondsLeft--
+                        binding.txv.text = secondsLeft.toString()
+                        binding.btnStart.isEnabled = false
+                        binding.btnStop.isEnabled = true
+                        delay(25)   //延遲0.025秒
+                    }
+                }
+
+            }
+        })
+
+        binding.btnStop.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(v: View?) {
+                //binding.txv.text = "結束倒數計時"
+
+                job.cancel()
+                binding.btnStart.isEnabled = true
+                binding.btnStop.isEnabled = false
+            }
+        })
     }
 }
