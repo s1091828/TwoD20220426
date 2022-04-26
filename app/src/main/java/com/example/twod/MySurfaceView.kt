e29f269370df2e271273103ccc32dadd566f31a1
Student <s1091828@gm.pu.edu.tw>
2022-04-26 11:46:12 +0800

    v7 : 使用SurfaceView

--- app/src/main/java/com/example/twod/MySurfaceView.kt
+++ app/src/main/java/com/example/twod/MySurfaceView.kt
@@ -1,8 +1,44 @@
 package com.example.twod
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
 import android.util.AttributeSet
+import android.view.SurfaceHolder
 import android.view.SurfaceView
 
-class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs) {
+class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs)
+    , SurfaceHolder.Callback {
+
+    lateinit var surfaceHolder: SurfaceHolder
+    lateinit var BG: Bitmap
+    lateinit var SuperMan:Bitmap
+
+    init {
+        surfaceHolder = getHolder()
+        BG = BitmapFactory.decodeResource(getResources(), R.drawable.back)
+        SuperMan = BitmapFactory.decodeResource(getResources(), R.drawable.superman)
+        surfaceHolder.addCallback(this)
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        var canvas: Canvas = surfaceHolder.lockCanvas()
+        drawSomething(canvas)
+        surfaceHolder.unlockCanvasAndPost(canvas)
+    }
+
+    fun drawSomething(canvas:Canvas) {
+        canvas.drawBitmap(BG, 0f, 0f, null)
+        canvas.drawBitmap(SuperMan, 100f, 100f, null)
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+
+    }
+
 }
